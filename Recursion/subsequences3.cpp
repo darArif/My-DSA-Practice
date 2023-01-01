50e7809e23df13579bca2a89a603f3aeac3aa6c0
darArif <arifbashirdar89@gmail.com>
2023-01-01 16:49:35 +0530

    Added: Printing Any One Subsquence with Sum k Program

---
+++ Recursion/subsequences3.cpp
@@ -0,0 +1,49 @@
+/*Printing Subsequences having sum equal to k) of an array using recursion and vectors*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+template<class T>
+void printSubs(vector<T> &subs)
+{
+    for(auto i=subs.begin(); i!=subs.end(); i++)
+        cout<<*i<<" ";
+    cout<<endl;
+}
+
+template<class T>
+void subSeq(int size, T arr[], int indx, vector<T> &subs)
+{
+    static int sum = 0;
+
+    if(indx==size) //including empty subsequences as well
+    {
+        if(sum == 5)
+        {
+            printSubs<T>(subs);
+            
+        }
+        return;
+    }
+
+    subs.push_back(arr[indx]);
+    sum += arr[indx];
+
+    subSeq(size, arr, indx+1, subs);
+    
+    subs.pop_back();
+    sum -= arr[indx];
+
+    subSeq(size, arr, indx+1, subs);
+}
+
+int main()
+{
+    int arr[10] = {-10, -7, -6, -3, -1, 0, 2, 4, 8, 9};
+
+    vector<int> subs;
+
+    subSeq<int>(10, arr, 0, subs);
+
+    return 0;
+}
